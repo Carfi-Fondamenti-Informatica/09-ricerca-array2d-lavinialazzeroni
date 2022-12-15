2574fdd7f6b7a9f03733e4584b04d8e683ec97eb
lavinialazzeroni <118049473+lavinialazzeroni@users.noreply.github.com>
2022-12-15 11:50:37 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,15 @@
-
+#include "lib.h"
+int funzione(char a [][20], char b [1][20]) {
+    for (int i=0; i<20; i++) {
+        if ( a [i][0] == b[0][0]) {
+            for (int j =0 ; j <20 ; j++) {
+                if (a [i][j]!= b[0][j]) {
+                    break;
+                }
+                if (j==19) {
+                    return i;
+                }
+            }
+        }
+    } return  -1;
+}
