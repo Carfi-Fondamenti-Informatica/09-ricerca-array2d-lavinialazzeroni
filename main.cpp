5277ebe3d0dca16b82353ae6bf0de0658aa4f8a3
lavinialazzeroni <118049473+lavinialazzeroni@users.noreply.github.com>
2022-12-15 11:52:21 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,23 @@
 #include <iostream>
-
-int main(){
-  return 0;
+using namespace std;
+#include "lib.h"
+int main() {
+    char array1 [10][20];
+    for (int i=0; i<10; i++) {
+        for(int j=0; j<20; j++) {
+            array1 [i][j]=' ';
+        }
+        cin>>array1[i];
+    }
+    char array2 [1] [20];
+        for(int j=0; j<20; j++) {
+            array2 [0][j]=' ';
+        }
+        cin>>array2[0];
+        if (funzione(array1, array2)==-1) {
+            cout<<"non presente"<<endl;
+        } else {
+            cout<<funzione(array1, array2) <<endl;
+        }
+    return 0;
 }
